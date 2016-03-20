e69c4616c46e4f87ae648720bfe5241c25fd47d0
Aidan Goldthorpe <aidan_goldthorpe@outlook.com>
2016-03-21 00:07:26 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+
+var currentTerm = "";
+var terms = [];
+var operators = [];
+evaulated = false; // Has the calculator just evalated a sum
+
+$(".number").click(function() { 
+	if (evaulated) {
+		// Clear the output
+		$("#output").html("");
+		evaulated = false;
+	}
+	currentTerm += $(this).html();
+	$("#output").html($("#output").html() + $(this).html());
+});
+
+$(".operator").click(function() {
+	if (currentTerm !== "") { 
+		// A number should be entered before an operator
+		terms.push(currentTerm);
+		currentTerm = "";
+		console.log(terms);
+		operators.push($(this).html());
+		$("#output").html($("#output").html() + $(this).html());		
+		console.log(operators);
+	}
+});
+
+$("#return").click(function() {
+	if (currentTerm === "") {
+		// The last entry was an operator, or there are no entries
+		return;
+	} else {
+		terms.push(currentTerm);
+		currentTerm = "";
+		console.log(terms);
+		console.log(operators);
+		// First evaluate the multiplaction and division
+		while ((mdIndex = operators.findIndex(function(oper) {return oper === "*" || oper === "/";})) >= 0) {
+			leftTerm = terms[mdIndex];
+			rightTerm = terms[mdIndex + 1]
+			if (operators[mdIndex] === "*") {
+				terms.splice(mdIndex, 2, leftTerm * rightTerm);
+			} else {
+				terms.splice(mdIndex, 2, leftTerm / rightTerm);
+			}
+			operators.splice(mdIndex, 1);
+		}
+		var sum = Number(terms.shift()), op = "";
+		// No multiplication or division, reduce the array
+		while (terms.length > 0) {
+			if ((op = operators.shift()) === "+") {
+				sum += Number(terms.shift());
+			} else if (op === "-") {
+				sum -= Number(terms.shift());
+			}
+		}
+		console.log(sum);
+		$("#output").html(sum); // Update the output
+		evaulated = true;
+	}
+})
